f1cbc0dd0ffc0bd834c2a6da2aaf9e03befd7217
krishnachaitanya018 <chaitanyaganni018@gmail.com>
2025-10-16 19:59:39 -0400

    commented the failed code in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 print("Hello World")
 print("This is Krishna")
 
-print("Starting…")
-raise RuntimeError("Boom! Intentional failure for CI demo")
-print("This never runs")
+#print("Starting…")
+#raise RuntimeError("Boom! Intentional failure for CI demo")
+#print("This never runs")
